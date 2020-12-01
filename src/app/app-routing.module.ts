f614c4ec47886fe7e519889385614515128069f1
farid <ansari.farid1@gmail.com>
2020-12-02 01:05:07 +0530

    add routing in Angular if u fire URL then component show

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+path:'',
+component:HomeComponent,
+pathMatch:'full'
+
+},
+{
+  path:'login',
+  component:LoginComponent,
+  pathMatch:'full'
+},
+{
+  path:'dashboard',
+  component:DashBoardComponent,
+  pathMatch:'full'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashBoardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
